597bb46f4aea957a50c103d0b905a3e1ac2de92c
Zhichaozhou1 <137164134+Zhichaozhou1@users.noreply.github.com>
2023-06-26 11:28:12 +0200

    Update lab.h

--- lab.h
+++ lab.h
@@ -24,15 +24,38 @@
 #include <time.h>
 
 void* recv_message (void *sock);
-void* send_message (char* message_send,void* sock);
+void* send_message (void *sock);
 void* process();
-int init_socket(char* IP_des);
+int init_socket(char* IP_des,int Port_src,char* message,char* pubkey_addr, char* prikey_addr);
+int PCGen(char* pubkey_addr, char* prikey_addr);
+int KeyGen(EC_KEY *ec_key, char* prikey_addr);
+int base64_encode(char in_str[], int in_len, char out_str[]);
+int base64_decode(char in_str[], int in_len, char out_str[]);
+int Sign(EC_KEY *ec_key, unsigned char *sig, const unsigned char digest[], int digest_len);
+int SignMain(EC_KEY *ec_key, unsigned char message[], unsigned char *signature, int *sig_len);
+int EVP(unsigned char message[],unsigned char digest[], unsigned int *digest_len);
+int message_sign(unsigned char beacon[], unsigned char base64message[], int flag, char* prikey_addr, char* pubkey_addr);
 typedef struct Link{
     char* str;
     struct Link *next;
 } queue;
+struct valid_PC{
+        struct valid_PC* next;
+        char* KeyID;
+        char* ts;
+        char* te;
+        char* pubkey;
+};
+struct HashTable_PC{
+        struct valid_PC** table;
+};
+
 queue * initLink();
 queue * insertElem(queue * p,char* msg);
-
-
+static unsigned int hash_33(char* key);
+struct HashTable_PC* hash_table_new();
+int hash_table_input(struct HashTable_PC* ht, char* key, char* ts, char* te, char* pubkey);
+int hash_table_get(struct HashTable_PC* ht, char* key, char* pubkey);
+int decryption(unsigned char* base64_receive, struct HashTable_PC* Pcert);
+int verify(EC_KEY *ec_key, const unsigned char *sig, int siglen, unsigned char message[]);
 #endif
