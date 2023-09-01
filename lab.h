bbde26cad5c69a8d596e35825f5f8f881b85b722
Zhichaozhou1 <137164134+Zhichaozhou1@users.noreply.github.com>
2023-09-01 17:39:24 +0200

    Update lab.h
    add more definitions

--- lab.h
+++ lab.h
@@ -22,19 +22,19 @@
 #include <openssl/pem.h>
 #include <openssl/buffer.h>
 #include <time.h>
-
+#define MAXSIGLEN 1024
 void* recv_message (void *sock);
 void* send_message (void *sock);
 void* process();
 int init_socket(char* IP_des,int Port_src,char* message,char* pubkey_addr, char* prikey_addr);
-int PCGen(char* pubkey_addr, char* prikey_addr);
+int PCGen(unsigned char* prikey_addr, unsigned char* pubkey_addr);
 int KeyGen(EC_KEY *ec_key, char* prikey_addr);
 int base64_encode(char in_str[], int in_len, char out_str[]);
 int base64_decode(char in_str[], int in_len, char out_str[]);
 int Sign(EC_KEY *ec_key, unsigned char *sig, const unsigned char digest[], int digest_len);
 int SignMain(EC_KEY *ec_key, unsigned char message[], unsigned char *signature, int *sig_len);
 int EVP(unsigned char message[],unsigned char digest[], unsigned int *digest_len);
-int message_sign(unsigned char beacon[], unsigned char base64message[], int flag, char* prikey_addr, char* pubkey_addr);
+int message_sign(unsigned char beacon[], unsigned char base64message[], int flag, unsigned char* prikey_addr, unsigned char* pubkey_addr);
 typedef struct Link{
     char* str;
     struct Link *next;
