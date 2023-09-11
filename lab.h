93d5a393d4163d57a02bac09d4f36ea351384be5
Zhichaozhou1 <137164134+Zhichaozhou1@users.noreply.github.com>
2023-09-11 16:54:37 +0200

    Update lab.h

--- lab.h
+++ lab.h
@@ -23,6 +23,8 @@
 #include <openssl/buffer.h>
 #include <time.h>
 #define MAXSIGLEN 1024
+#define TABLE_SIZE 1000
+#define rsu_key_initial "1d5c6ed404b9a1f19e6830a098cd481f08a8f6355993a5e89a1067808d058a86"
 void* recv_message (void *sock);
 void* send_message (void *sock);
 void* process();
@@ -35,10 +37,7 @@ int Sign(EC_KEY *ec_key, unsigned char *sig, const unsigned char digest[], int d
 int SignMain(EC_KEY *ec_key, unsigned char message[], unsigned char *signature, int *sig_len);
 int EVP(unsigned char message[],unsigned char digest[], unsigned int *digest_len);
 int message_sign(unsigned char beacon[], unsigned char base64message[], int flag, unsigned char* prikey_addr, unsigned char* pubkey_addr);
-typedef struct Link{
-    char* str;
-    struct Link *next;
-} queue;
+
 struct valid_PC{
         struct valid_PC* next;
         char* KeyID;
@@ -46,16 +45,23 @@ struct valid_PC{
         char* te;
         char* pubkey;
 };
+
 struct HashTable_PC{
         struct valid_PC** table;
 };
+
+typedef struct Link{
+    char* str;
+    struct Link *next;
+} queue;
 
 queue * initLink();
 queue * insertElem(queue * p,char* msg);
 static unsigned int hash_33(char* key);
 struct HashTable_PC* hash_table_new();
-int hash_table_input(struct HashTable_PC* ht, char* key, char* ts, char* te, char* pubkey);
+int hash_table_input(struct HashTable_PC* ht, unsigned char* key, unsigned char* ts, unsigned char* te, unsigned char* pubkey);
 int hash_table_get(struct HashTable_PC* ht, char* key, char* pubkey);
 int decryption(unsigned char* base64_receive, struct HashTable_PC* Pcert);
 int verify(EC_KEY *ec_key, const unsigned char *sig, int siglen, unsigned char message[]);
+int message_process(unsigned char base64_receive[], struct HashTable_PC* ht);
 #endif
